811a48bb5cb34208d84946fa8a79a5f946caae9c
rhofp <rhodfra@gmail.com>
2020-08-03 10:39:53 -0500

    New list of basic config files

--- sync.sh
+++ sync.sh
@@ -1,9 +1,9 @@
 # First we copy files
-cp ~/.vimrc ~/.profile ~/.bashrc ~/.zshrc ~/.zsh_history ~/.bash_history ~/.bash_profile ~/.Xresources ~/.zhistory .xinitrc .Xauthority .
+cp ~/.vimrc ~/.profile ~/.bashrc ~/.zshrc ~/.zsh_history ~/.bash_history ~/.bash_profile ~/.Xresources ~/.zhistory ~/.xinitrc ~/.Xauthority .
 
 
 # Copyng directories
 cp -r ~/.i3/ .
 mkdir .config
-cp -r ~/.config/{i3,mimeapps.list,dmenu-recent} .config 
+cp -r ~/.config/{mimeapps.list,dmenu-recent} .config 
 cp -r ~/.images .
