8d267f1b008e5549ff286033754cccad22a166cc
rhofp <rhodfra@gmail.com>
2021-05-12 23:58:29 -0500

    Added AWESOME WM configuration files
    This files include some apps downloaded form the freedesktop org and the
    lain github repository along with 4 themes for the entire WM.
    
    This commit include all the neccessary imags for the bar and other
    possible widgets

---
+++ .config/awesome/autostart.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+#run xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
+#run xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+#run caffeine
+#run pamac-tray
+run nm-applet
+run variety
+run xfce4-power-manager
+run blueberry-tray
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+run numlockx on
+run volumeicon
+feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+run flameshot
+run shutter --min_at_startup
+#run nitrogen --restore
+#run conky -c $HOME/.config/awesome/system-overview
+
+#run applications from startup
+#run dropbox
+#run insync start
+#run spotify
+#run ckb-next -b
+
+#run telegram-desktop &
+#run discord &
+#run zoom &
+#run skypeforlinux &
+
+#run brave &
